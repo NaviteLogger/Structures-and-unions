fa76df3d5d905e4d2fa1c83ff3729370d624c45c
NaviteLogger5547 <kacprzakmarek92@gmail.com>
2023-01-10 19:51:02 +0100

    Commit of the files

---
+++ main.c
@@ -0,0 +1,74 @@
+// #include <stdio.h>
+
+// typedef union PrzechowalniaDanych
+// {
+//     char imie[20];
+//     char nazwisko[30];
+//     char rodzajDokumentuTozsamosci;
+//     int numerDokumentuTozsamosci[10];
+// } PrzechowalniaDanych;
+
+// int main()
+// {
+//     PrzechowalniaDanych uniaTestowa;
+//     printf("Podaj swoje imie: ");
+//     scanf("%s", &uniaTestowa.imie);
+//     printf("Podaj swoje nazwisko: ");
+//     scanf("%s", &uniaTestowa.nazwisko);
+//     printf("Podaj swoj rodzaj dokumentu: ");
+//     fflush(stdin);
+//     scanf("%c", &uniaTestowa.rodzajDokumentuTozsamosci);
+//     fflush(stdin);
+//     printf("Podaj swoj numer dokumentu: ");
+//     scanf("%i", &uniaTestowa.numerDokumentuTozsamosci);
+
+//     printf("Imie: %s, nazwisko: %s, rodzaj dokumentu tozsamosci: %c, numer dokumentu tozsamosci: %i", uniaTestowa.imie, uniaTestowa.nazwisko, uniaTestowa.rodzajDokumentuTozsamosci, uniaTestowa.numerDokumentuTozsamosci);
+//     return 0;
+// }
+
+#include <stdio.h>
+
+typedef struct PrzechowalniaDanych
+{
+    char imie[20];
+    char nazwisko[30];
+    char rodzajDokumentuTozsamosci;
+    int numerDokumentuTozsamosci[10];
+} PrzechowalniaDanych;
+
+int main()
+{
+    PrzechowalniaDanych struktura;
+    PrzechowalniaDanych strukturaKopia;
+
+    strukturaKopia = struktura;
+
+    char tablicaWyrazow[][30] =
+        {
+            "Dane osoby: ",
+            "Nazwisko: ",
+            "Imie: ",
+            "Rodzaj dokumentu:",
+            "oznaczenia: ",
+            "P-Paszport",
+            "D-Dowod osobisty",
+            "K-Karta pobytu"
+            "Numer dokumentu"};
+
+    printf("Podaj swoje imie: ");
+    scanf("%s", &struktura.imie);
+    printf("Podaj swoje nazwisko: ");
+    scanf("%s", &struktura.nazwisko);
+    printf("Podaj swoj rodzaj dokumentu: ");
+    fflush(stdin);
+    scanf("%c", &struktura.rodzajDokumentuTozsamosci);
+    fflush(stdin);
+    printf("Podaj swoj numer dokumentu: ");
+    scanf("%i", &struktura.numerDokumentuTozsamosci);
+
+    printf("%s", tablicaWyrazow[0]);
+    printf("%s, ");
+    printf("Imie: %s, nazwisko: %s, rodzaj dokumentu tozsamosci: %c, numer dokumentu tozsamosci: %i", struktura.imie, struktura.nazwisko, struktura.rodzajDokumentuTozsamosci, struktura.numerDokumentuTozsamosci);
+
+    return 0;
+}
